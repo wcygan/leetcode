18669de403ea3d25d42637264180f941449641fb
Will Cygan <wcygan.io@gmail.com>
2023-11-26 09:52:37 -0600

    p104 [easy]

---
+++ lib/src/main/java/leetcode/easy/p104_maxtreedepth/Solution.java
@@ -0,0 +1,27 @@
+package leetcode.easy.p104_maxtreedepth;
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int l = maxDepth(root.left);
+        int r = maxDepth(root.right);
+
+        return 1 + Math.max(l, r);
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
